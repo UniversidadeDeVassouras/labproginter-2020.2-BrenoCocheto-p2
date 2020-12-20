898ce629fbcf0572ced155908718a2e207bff8be
Breno Cocheto <48560880+bcocheto@users.noreply.github.com>
2020-12-20 11:53:21 -0300

    Adicionada Subject ao __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__,
-            static_folder=os.path.abspath("application/view/static"),
-            template_folder=os.path.abspath("application/view/templates"),)
+app = Flask(
+    __name__,
+    static_folder=os.path.abspath("application/view/static"),
+    template_folder=os.path.abspath("application/view/templates"),
+)
 
 from application.controller.home_controller import home
+from application.controller.subject_controller import subject
